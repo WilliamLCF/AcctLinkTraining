2ab1e1434dc2b26975a5ecd0ebfa02c06760279c
WilliamLi <WilliamLi@tw.ibm.com>
2019-05-05 20:04:08 +0800

    範例

---
+++ example/src/main/java/com/esun/example/web/controller/HelloWorldController.java
@@ -0,0 +1,38 @@
+package com.esun.example.web.controller;
+ 
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.esun.example.model.HelloWorld;
+
+@Controller
+public class HelloWorldController {
+	@GetMapping("/home")
+	public String getHome(Model model) {
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.setMessage("Hello World Example Using Spring MVC 5!!!");
+		helloWorld.setDateTime(LocalDateTime.now().toString());
+		model.addAttribute("helloWorld", helloWorld); 
+		return "index.html";
+	}
+
+	@GetMapping("/markup")
+	public String markupPage() {
+		return "markup.html";
+	}
+
+	@GetMapping("/params")
+	public String paramsPage() {
+		return "params.html";
+	}
+
+	@GetMapping("/other")
+	public String otherPage(Model model) {
+		return "other.html";
+	}
+
+}
